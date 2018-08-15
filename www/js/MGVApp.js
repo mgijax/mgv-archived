4ca46448a95d8c221c272126293de6a5c316b8a2
Joel Richardson <joel.richardson@jax.org>
2018-08-15 05:52:23 -0400

    Tweak info button position.

--- www/js/MGVApp.js
+++ www/js/MGVApp.js
@@ -176,16 +176,16 @@ class MGVApp extends Component {
 		.attr('class','material-icons busy rotating')
 	    ;
 	//
-	// If a pagebox has title text, append a help icon and move the text there
-	d3.selectAll('.pagebox[title]')
+	// If a pagebox has title text, append a help icon to the label and move the text there
+	d3.selectAll('.pagebox[title] > label')
 	    .append('i')
 	        .attr('title', function(){
-		    let p = d3.select(this.parentNode);
+		    let p = d3.select(this.parentNode.parentNode);
 		    let t = p.attr('title');
 		    p.attr('title', null);
 		    return t;
 		})
-	        .attr('class', 'material-icons button help')
+	        .attr('class', 'material-icons help')
 		;
 	//
 	d3.selectAll('.closable')
