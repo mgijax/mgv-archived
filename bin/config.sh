2f5ebca8c1d57a1b87a5fcccf57a937102a86dfc
Joel Richardson <joel.richardson@jax.org>
2018-09-19 16:12:38 -0400

    Make python a variable.

--- bin/config.sh
+++ bin/config.sh
@@ -5,5 +5,7 @@ BDIR="${DIR}/bin"
 GDIR="${DIR}/data/genomedata"
 LOG="${DIR}/data/LOG"
 
+PYTHON="python"
+
 # Max size in Mb for a feature. Features above this size are removed and reported.
 export SIZELIMIT="10"
